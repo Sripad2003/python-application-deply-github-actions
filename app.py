10591997740a52b4c4f47b339fd3e68d6f40b751
Sripad2003 <joshisripad999@gmail.com>
2026-06-17 13:25:27 +0530

    changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello! Welcome to my Flask application."
+    return "Hello! Welcome to my Flask application version 1.0."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
